f681c425eb625a51315b7d638de88015c0f81291
TiagoDanin <TiagoDanin@outlook.com>
2018-04-16 19:32:12 -0300

    Update test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,5 +3,11 @@ rm source/meta.json -f
 echo '{
 	"baseURL": ""
 }' > source/meta.json
-echo "Start jus"
+echo "Fix Jus Ignore"
+bash scripts/fixJus.sh
+echo "Get Data Github and NPM"
+bash scripts/updateData.sh
+bash scripts/getProjectsGithub.sh
+node scripts/getProjectsNPM.js
+echo "Start with jus"
 npx jus server source

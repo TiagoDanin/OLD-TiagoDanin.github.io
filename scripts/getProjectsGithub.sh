1cb7e3192b8ac5b15d8fb91ef8bb8b1b4d1a968b
Tiago Danin <TiagoDanin@outlook.com>
2021-02-28 14:30:05 -0300

    feat: add more links, and new buttom menu

--- scripts/getProjectsGithub.sh
+++ scripts/getProjectsGithub.sh
@@ -1,2 +1,2 @@
-wget "https://api.github.com/users/TiagoDanin/repos?page=0&per_page=100" --show-progress -O source/projects/github.json
+wget "https://api.github.com/users/TiagoDanin/repos?page=0&per_page=200" --show-progress -O source/projects/github.json
 cp source/projects/github.json source/
